9cad1ee964b58a494a6173e89e467ea8361296d7
razonyang <razonyang@gmail.com>
2023-01-26 20:36:29 +0800

    Move cache dependency to service

--- internal/github/service.go
+++ internal/github/service.go
@@ -1,6 +1,8 @@
 package github
 
 import (
+	"context"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"io"
@@ -10,48 +12,73 @@ import (
 	"strconv"
 	"strings"
 	"time"
+
+	"github.com/eko/gocache/lib/v4/cache"
+	"github.com/eko/gocache/lib/v4/store"
+	log "github.com/sirupsen/logrus"
 )
 
 var regRepos = regexp.MustCompile(`([\d,]+)\s+Repositories`)
 var regPkgs = regexp.MustCompile(`([\d,]+)\s+Packages`)
 
 type Service struct {
+	cache cache.CacheInterface[string]
 }
 
-func NewService() *Service {
-	return &Service{}
+func NewService(cache cache.CacheInterface[string]) *Service {
+	return &Service{
+		cache: cache,
+	}
 }
 
-func (s *Service) DependentsByOwnerAndRepo(owner, repo string) (*Dependents, error) {
-	api := fmt.Sprintf("%s/%s/%s/network/dependents", os.Getenv("GITHUB_URL"), owner, repo)
-	client := http.Client{
-		Timeout: 60 * time.Second,
+func (s *Service) Dependents(ctx context.Context, owner, repo string) (*Dependents, error) {
+	d := &Dependents{}
+	cacheKey := fmt.Sprintf("github-dependents -%s-%s", owner, repo)
+	cachedVal, err := s.cache.Get(ctx, cacheKey)
+	if err == nil {
+		err = json.Unmarshal([]byte(cachedVal), d)
 	}
-	resp, err := client.Get(api)
+
 	if err != nil {
-		return nil, err
-	}
+		log.Debugf("failed to fetch dependents from cache: %s", err)
 
-	if resp.StatusCode != 200 {
-		return nil, errors.New(resp.Status)
-	}
+		api := fmt.Sprintf("%s/%s/%s/network/dependents", os.Getenv("GITHUB_URL"), owner, repo)
+		client := http.Client{
+			Timeout: 60 * time.Second,
+		}
+		resp, err := client.Get(api)
+		if err != nil {
+			return nil, err
+		}
 
-	defer resp.Body.Close()
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return nil, err
-	}
+		if resp.StatusCode != 200 {
+			return nil, errors.New(resp.Status)
+		}
 
-	d := &Dependents{0, 0}
+		defer resp.Body.Close()
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			return nil, err
+		}
 
-	repoMatches := regRepos.FindAllSubmatch(body, 1)
-	if len(repoMatches) > 0 {
-		d.Repositories, _ = strconv.Atoi(strings.ReplaceAll(string(repoMatches[0][1]), ",", ""))
-	}
+		repoMatches := regRepos.FindAllSubmatch(body, 1)
+		if len(repoMatches) > 0 {
+			d.Repositories, _ = strconv.Atoi(strings.ReplaceAll(string(repoMatches[0][1]), ",", ""))
+		}
 
-	pkgMatches := regPkgs.FindAllSubmatch(body, 1)
-	if len(pkgMatches) > 0 {
-		d.Packages, _ = strconv.Atoi(strings.ReplaceAll(string(pkgMatches[0][1]), ",", ""))
+		pkgMatches := regPkgs.FindAllSubmatch(body, 1)
+		if len(pkgMatches) > 0 {
+			d.Packages, _ = strconv.Atoi(strings.ReplaceAll(string(pkgMatches[0][1]), ",", ""))
+		}
+
+		data, err := json.Marshal(d)
+		if err != nil {
+			log.Infof("failed to marshal dependents: %s", err)
+		} else {
+			if err = s.cache.Set(ctx, cacheKey, string(data), store.WithExpiration(6*time.Hour)); err != nil {
+				log.Infof("failed to cache: %s", err)
+			}
+		}
 	}
 
 	return d, nil
